82a104482fc3e926976d26055f389cab9205f6da
Yifu Yao <yveyao@outlook.com>
2020-07-12 01:35:44 -0500

    connect todos with H2 database by JPA

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values(10001, 'admin', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10002, 'admin', 'Learn data jpa', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10003, 'admin', 'Learn web service', sysdate(), false);
--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -1,26 +1,26 @@
 import axios from "axios"
-import {API_URL} from "../../Constants.js"
+import {JPA_API_URL, API_URL} from "../../Constants.js"
 
 class TodoDataService{
 
     retrieveAllTodos(username){
-        return axios.get(`${API_URL}/users/${username}/todos`)
+        return axios.get(`${JPA_API_URL}/users/${username}/todos`)
     }
 
     deteleTodo(name, id){
-        return axios.delete(`${API_URL}/users/${name}/todos/${id}`)
+        return axios.delete(`${JPA_API_URL}/users/${name}/todos/${id}`)
     }
 
     retrieveTodo(name, id){
-        return axios.get(`${API_URL}/users/${name}/todos/${id}`)
+        return axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`)
     }
 
     updateTodo(name, id, todo){
-        return axios.put(`${API_URL}/users/${name}/todos/${id}`, todo)
+        return axios.put(`${JPA_API_URL}/users/${name}/todos/${id}`, todo)
     }
 
     createTodo(name, todo){
-        return axios.post(`${API_URL}/users/${name}/todos`, todo)
+        return axios.post(`${JPA_API_URL}/users/${name}/todos`, todo)
     }
 }
 
